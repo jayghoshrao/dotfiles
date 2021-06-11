59b023606fc3e8a4e30b3638f0138ff6b088c849
Jayghosh Rao <jayghoshter@gmail.com>
2021-06-11 14:27:28 +0200

    Script updates

--- bin/muttrangerpick.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-tmpfile=/tmp/muttrangerpick
-if \[ -z "$1" \]; then
-   ranger --choosefiles $tmpfile && sed -i 's/ /\\ /g' $tmpfile && echo "$(awk 'BEGIN {printf "%s", "push "} {printf "%s", "<attach-file>"$0"<enter>"}'  $tmpfile)" > $tmpfile
-elif \[ $1 == "clean" \]; then
-  rm $tmpfile
-fi
