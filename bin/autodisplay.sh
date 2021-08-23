e8330600de69c8f1a39292ba481f960431dcd831
Jayghosh Rao <jayghoshter@gmail.com>
2021-08-23 23:34:58 +0530

    Update bin scripts

--- bin/autodisplay.sh
+++ bin/autodisplay.sh
@@ -5,25 +5,25 @@
 
 set -euo pipefail
 
-run -d
+# run -d
 
-# layoutsDir="$HOME/.screenlayout"
-# layoutsFile="$layoutsDir/layouts"
-# currentHash=$(xrandr --prop | grep -A2 EDID | sha256sum | cut -c 1-32)
-# currentLayout=$(grep "$currentHash" "$layoutsFile" | awk '{print $1}')
+layoutsDir="$HOME/.screenlayout"
+layoutsFile="$layoutsDir/layouts"
+currentHash=$(xrandr --prop | grep -A2 EDID | sha256sum | cut -c 1-32)
+currentLayout=$(grep "$currentHash" "$layoutsFile" | awk '{print $1}')
 
-# # ## Turn off everything except laptop monitor
-# xrandr \
-# --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
-# --output VIRTUAL1 --off  \
-# --output DP1 --off  \
-# --output HDMI2 --off  \
-# --output HDMI1 --off  \
-# --output DP1-3 --off  \
-# --output DP1-2 --off  \
-# --output DP1-1 --off  \
-# --output DP2 --off \
-# --output DP2-2 --off \
-# --output DP2-3 --off 
+# ## Turn off everything except laptop monitor
+xrandr \
+--output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+--output VIRTUAL1 --off  \
+--output DP1 --off  \
+--output HDMI2 --off  \
+--output HDMI1 --off  \
+--output DP1-3 --off  \
+--output DP1-2 --off  \
+--output DP1-1 --off  \
+--output DP2 --off \
+--output DP2-2 --off \
+--output DP2-3 --off 
 
-# "$layoutsDir/${currentLayout}.sh"
+"$layoutsDir/${currentLayout}.sh"
