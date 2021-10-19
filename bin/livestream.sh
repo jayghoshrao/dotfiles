596a3e2cc7d9e96820ceb1740b4b89812358bae4
Jayghosh Rao <jayghoshter@gmail.com>
2021-10-19 23:10:46 +0200

    update scripts

--- bin/livestream.sh
+++ bin/livestream.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 # streamlink -p "mpv --no-audio --wid=$2" "$1" best
 # streamlink -p "mpv --hwdec=vaapi --wid=$2" "$1" best
-eval "mpv --hwdec=vaapi --wid=$2 $1"
+eval "mpv --volume=50 --hwdec=vaapi --wid=$2 $1"
+# eval "mpv --wid=$2 $1"
