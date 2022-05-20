e991d47d09c07eef3bf083c64d655ccff03b0f41
Jayghosh Rao <jayghoshter@gmail.com>
2022-05-20 15:27:01 +0200

    roll.py: implement h,l modifiers. allow a+5, z+2 notation for advantage disadvantage d20

--- bin/roll.py
+++ bin/roll.py
@@ -5,26 +5,54 @@ from rich import print as rprint
 from rich.text import Text
 import argparse
 from functools import partial
+import enum
 
 ## TODO: Make multiroll return a list instead of printing
 
-def multiroll(string:str, **kwargs):
+class Mode(enum.Enum):
+    DISADVANTAGE = -1
+    NORMAL = 0
+    ADVANTAGE = 1
+
+    @classmethod
+    def from_string(cls, string: str) -> 'Mode':
+        """Get an enum value from a string, defaulting to NORMAL."""
+        encode = {
+            'normal': cls.NORMAL,
+            'advantage': cls.ADVANTAGE,
+            'disadvantage': cls.DISADVANTAGE,
+            'adv': cls.ADVANTAGE,
+            'dis': cls.DISADVANTAGE,
+            'disadv': cls.DISADVANTAGE,
+        }
+        return encode.get(string.lower(), cls.NORMAL)
+
+def multiroll(string:str, silent:bool=False, mode:Mode=Mode.NORMAL):
     """
     Accepts advanced multiple rolls separated by '|'
         - ad20 | +5 | 1d3
     """
-    _silent = kwargs.get('silent', False)
+
     string = string.split(';')[0]
-    for item in string.split('|'):
+    results = []
+    for index,item in enumerate(string.split('|')):
         text = Text()
         rollstr = item.strip()
-        rollval = roll(str(rollstr), silent=_silent)
+        if index == 0:
+            if mode == Mode.ADVANTAGE:
+                rollstr = 'a' + rollstr
+            elif mode == Mode.DISADVANTAGE: 
+                rollstr = 'z' + rollstr
+        rollval = roll(str(rollstr), silent=silent)
         text.append(rollstr + ": ", style="bold white")
         text.append(str(rollval), style="bold cyan")
-        if not _silent:
+        if not silent:
             rprint(text)
-        else:
-            print(rollval)
+        # else:
+        #     print(rollval)
+        results.append(rollval)
+    return results
+
 
 def roll(string:str, **kwargs):
     """
@@ -32,13 +60,28 @@ def roll(string:str, **kwargs):
         - 1d5 + 10
         - ad20
         - 10d50/3
-        - +5 (Rolls d20+5)
+        - +5 (rolls d20+5)
+        - a+5 (rolls ad20+5 )
     """
     _silent = kwargs.get('silent', False)
     roller = partial(roll, silent=_silent)
     roller_nds = partial(roll_nds, silent=_silent)
 
-    if ';' in string:
+    if(string[0] == 'a'):
+        if not _silent:
+            rprint(Text("Rolling with Advantage!", style="bold blue"))
+        r1 = roller(string[1:])
+        r2 = roller(string[1:])
+        return [max(r1, r2), (r1,r2)]
+    elif(string[0] == 'z'):
+        if not _silent:
+            rprint(Text("Rolling with Disadvantage!", style="bold blue"))
+        r1 = roller(string[1:])
+        r2 = roller(string[1:])
+        # return min(r1, r2)
+        return [min(r1, r2), (r1,r2)]
+    elif ';' in string:
+        # Everything after ; is a comment.
         splitstring=string.split(';')
         return roller(splitstring[0])
     elif string.strip() == '':
@@ -63,24 +106,7 @@ def roll(string:str, **kwargs):
         splitstring = string.split('-')
         return roller(splitstring[0]) - roller(splitstring[1])
     else:
-        # try:
-        if(string[0] == 'a'):
-            if not _silent:
-                rprint(Text("Rolling with Advantage!", style="bold blue"))
-            r1 = roller_nds(string[1:])
-            r2 = roller_nds(string[1:])
-            return max(r1, r2)
-        elif(string[0] == 'z'):
-            if not _silent:
-                rprint(Text("Rolling with Disadvantage!", style="bold blue"))
-            r1 = roller_nds(string[1:])
-            r2 = roller_nds(string[1:])
-            return min(r1, r2)
-        else:
-            return roller_nds(string)
-        # except:
-        #     print("Something is wrong. Rolling 0.")
-        #     return 0
+        return roller_nds(string)
 
 def roll_nds(string:str, **kwargs):
     """
@@ -90,7 +116,25 @@ def roll_nds(string:str, **kwargs):
         - (num)d(sides)
     """
     seed(a=None, version=2)
-    if 'd' in string:
+    if 'h' in string: 
+        rollstr = string.split('h')[0]
+        highnum = int(string.split('h')[1])
+        n = int(rollstr.split('d')[0])
+        s = int(rollstr.split('d')[1])
+        rolls = [ randint(1,s) for _ in range(n)]
+        rolls.sort(reverse=True)
+        rolls = rolls[:highnum]
+        return(sum(rolls))
+    elif 'l' in string: 
+        rollstr = string.split('h')[0]
+        lownum = int(string.split('h')[1])
+        n = int(rollstr.split('d')[0])
+        s = int(rollstr.split('d')[1])
+        rolls = [ randint(1,s) for _ in range(n)]
+        rolls.sort()
+        rolls = rolls[:lownum]
+        return(sum(rolls))
+    elif 'd' in string:
         try:
             n = int(string.split('d')[0])
         except:
