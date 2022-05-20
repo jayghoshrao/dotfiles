eab968f87226eb39475df4b2087c2292ab0b5176
Jayghosh Rao <jayghoshter@gmail.com>
2022-05-20 15:23:11 +0200

    update scripts

--- bin/trim-resize-batch.sh
+++ bin/trim-resize-batch.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
-mkdir trimmed-resized
-mogrify -path trimmed-resized/ -trim -background none -gravity center -extent 105%x102% "$@"
+BG=none
+EXTENT=105%x102%
+OUTDIR=trimmed-resized
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -b|--background)
+            BG=$2
+            shift # past value
+            shift # past value
+            ;;
+        -e|--extent)
+            EXTENT=$2
+            shift # past value
+            shift # past value
+            ;;
+        *)    # unknown option
+            POSITIONAL+=("$1") # save it in an array for later
+            shift # past argument
+            ;;
+    esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+mkdir -p "$OUTDIR"
+mogrify -path "$OUTDIR/" -trim -background $BG -gravity center -extent $EXTENT "$@"
