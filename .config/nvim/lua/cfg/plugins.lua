717c538c144e01171edf3cf93913e7347c8940a2
Jayghosh Rao <jayghoshter@gmail.com>
2022-06-23 17:09:23 +0200

    updates

--- .config/nvim/lua/cfg/plugins.lua
+++ .config/nvim/lua/cfg/plugins.lua
@@ -48,7 +48,7 @@ return require('packer').startup(function(use)
     -- Advanced highlighting
     use {
         'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate',
+        
         config = function()
             require 'cfg.plugins.treesitter'
         end,
@@ -360,25 +360,25 @@ return require('packer').startup(function(use)
                     edit = "e",
                     repl = "r",
                 },
-                sidebar = {
-                    -- You can change the order of elements in the sidebar
-                    elements = {
-                        -- Provide as ID strings or tables with "id" and "size" keys
-                        {
-                            id = "scopes",
-                            size = 0.25, -- Can be float or integer > 1
+                layouts = {
+                    {
+                        elements = {
+                            'scopes',
+                            'breakpoints',
+                            'stacks',
+                            'watches',
                         },
-                        { id = "breakpoints", size = 0.25 },
-                        { id = "stacks", size = 0.25 },
-                        { id = "watches", size = 00.25 },
+                        size = 40,
+                        position = 'left',
                     },
-                    size = 40,
-                    position = "left", -- Can be "left", "right", "top", "bottom"
-                },
-                tray = {
-                    elements = { "repl" },
-                    size = 10,
-                    position = "bottom", -- Can be "left", "right", "top", "bottom"
+                    {
+                        elements = {
+                            'repl',
+                            'console',
+                        },
+                        size = 10,
+                        position = 'bottom',
+                    },
                 },
                 floating = {
                     max_height = nil, -- These can be integers or a float between 0 and 1.
@@ -392,6 +392,25 @@ return require('packer').startup(function(use)
             })
             require('cfg.utils').map('n', '<space>dd', '<cmd>lua require("dapui").toggle()<cr>')
             require('cfg.utils').map('n', '<space>dD', '<cmd>lua require("dapui").close()<cr>')
+        end
+    }
+
+    use {
+        'theHamsta/nvim-dap-virtual-text',
+        config = function()
+            require("nvim-dap-virtual-text").setup({
+                enabled = true,                     -- enable this plugin (the default)
+                enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+                highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+                highlight_new_as_changed = false,   -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+                show_stop_reason = true,            -- show stop reason when stopped for exceptions
+                commented = false,                  -- prefix virtual text with comment string
+                -- experimental features:
+                virt_text_pos = 'eol',              -- position of virtual text, see `:h nvim_buf_set_extmark()`
+                all_frames = false,                 -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+                virt_lines = false,                 -- show virtual lines instead of virtual text (will flicker!)
+                virt_text_win_col = nil             -- position the virtual text at a fixed window column (starting from the first text column) ,
+            })
         end
     }
 
@@ -581,9 +600,17 @@ return require('packer').startup(function(use)
 
     use 'duane9/nvim-rg'
 
-    use 'kdheepak/lazygit.nvim'
+    use {
+        'kdheepak/lazygit.nvim',
+        config = function()
+            local map = require('cfg.utils').map
+            map('n', '<leader>gg', [[<cmd>LazyGit<cr>]])
+        end
+    }
 
     use 'LnL7/vim-nix'
+
+    use 'edluffy/hologram.nvim'
 
 end)
 
