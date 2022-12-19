9cdbcdc1ea70fbbf9fbe315e23ebb03a8672dd04
Jayghosh Rao <jayghoshter@gmail.com>
2022-12-19 14:11:28 +0100

    implement packer bootstrap as in GitHub readme

--- .config/nvim/lua/cfg/plugins.lua
+++ .config/nvim/lua/cfg/plugins.lua
@@ -30,16 +30,28 @@ local cmd = vim.cmd
 
 
 -- -- Automatically install packer.nvim
-local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-    cmd('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+-- local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+-- if fn.empty(fn.glob(install_path)) > 0 then
+--     cmd('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+-- end
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
+
 
 --- startup and add configure plugins
 -- packer.startup(function()
 return require('packer').startup(function(use)
-    local use = use
-
     use 'wbthomason/packer.nvim'
 
     -- personal
@@ -623,6 +635,12 @@ return require('packer').startup(function(use)
         -- end
     }
     vim.g.jupytext_filetype_map = {md = 'vimwiki'}
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+    require('packer').sync()
+    end
 
 end)
 
