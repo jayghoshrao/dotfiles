61e4d722e45e45e5d737488ccb1123fb78ac17c9
Jayghosh Rao <jayghoshter@gmail.com>
2023-01-27 13:32:26 +0100

    fold text in scope.sh

--- bin/scope.sh
+++ bin/scope.sh
@@ -21,9 +21,9 @@ elif [[ "$FILETYPE" == *"directory"*  ]]; then
 elif [ "$EXT" = "pdf" ]; then
 	v=$(echo "$KEY" | tr " " "|")
 	echo {$FILE_EXT} | "$GREPPRG" -E "^|$v" -i --color=always
-	pdftotext -f 1 -l 1 "$FILE_EXT" - | "$GREPPRG" -E "^|$v" -i --color=always 
+	pdftotext -f 1 -l 1 "$FILE_EXT" - | fold -w 80 -s | "$GREPPRG" -E "^|$v" -i --color=always 
 elif [[ "$FILETYPE" == *"text"*  ]]; then
-	cat "$FILE_EXT"
+	fold -w 80 -s "$FILE_EXT" 
 elif [[ "$FILETYPE" == *"image"*  ]]; then
     # termplay "$FILE_EXT"
     # img.sh -d "$FILE_EXT"
