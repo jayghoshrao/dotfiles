ae1676d813b81d3a3f9a051b267ba67f8bd21632
Jayghosh Rao <jayghoshter@gmail.com>
2023-06-04 11:06:00 +0200

    use make instead of cmake for telescope-fzf

--- .config/nvim/lua/cfg/plugins_core.lua
+++ .config/nvim/lua/cfg/plugins_core.lua
@@ -158,7 +158,8 @@ return {
             -- { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
             {
                 'nvim-telescope/telescope-fzf-native.nvim', 
-                build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' 
+                -- build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' 
+                build = 'make' 
             }
         },
     },
