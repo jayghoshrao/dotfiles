9fa01db53c81d0c0bdd13f2637c514c569396a8e
Jayghosh Rao <jayghoshter@gmail.com>
2023-08-20 17:13:19 +0200

    give up on trying to conditionally build fzf-native

--- .config/nvim/lua/cfg/plugins_core.lua
+++ .config/nvim/lua/cfg/plugins_core.lua
@@ -200,39 +200,30 @@ return {
             'nvim-lua/plenary.nvim', -- Useful Lua utilities
             'nvim-lua/popup.nvim',
             'nvim-telescope/telescope-github.nvim',
-            -- FZY sorter for Telescope
-            -- 'nvim-telescope/telescope-fzy-native.nvim'
-            -- TODO: FZF sorter for Telescope
-            -- { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
-            {
-                'nvim-telescope/telescope-fzf-native.nvim', 
-                enabled = function()
-                    return vim.fn.executable('cmake') == 1 or vim.fn.executable('make') == 1
-                end,
-                build = function()
-                    if vim.fn.executable('make') then
-                        os.execute('make')
-                    elseif vim.fn.executable('cmake') then
-                        os.execute('cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build')
-                    end
-                end,
-                -- build = 'make' 
-                config = function()
-                    require('telescope').setup {
-                        extensions = {
-                            fzf = {
-                                fuzzy = true,                    -- false will only do exact matching
-                                override_generic_sorter = true,  -- override the generic sorter
-                                override_file_sorter = true,     -- override the file sorter
-                                case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                -- the default case_mode is "smart_case"
-                            }
-                        }
-                    }
-                    require('telescope').load_extension('fzf')
-                end
-            }
         },
+    },
+
+    {
+        'nvim-telescope/telescope-fzf-native.nvim', 
+        enabled = function()
+            return vim.fn.executable('make') == 1
+        end,
+        build = 'make',
+        -- build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+        config = function()
+            require('telescope').setup {
+                extensions = {
+                    fzf = {
+                        fuzzy = true,                    -- false will only do exact matching
+                        override_generic_sorter = true,  -- override the generic sorter
+                        override_file_sorter = true,     -- override the file sorter
+                        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                        -- the default case_mode is "smart_case"
+                    }
+                }
+            }
+            require('telescope').load_extension('fzf')
+        end
     },
 
     {
