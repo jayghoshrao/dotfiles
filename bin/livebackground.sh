6fb859147a522b86dcabc322e6b0a9723c9babe0
Jayghosh Rao <jayghoshter@gmail.com>
2024-01-05 23:33:45 +0100

    allow selection of monitor for live background

--- bin/livebackground.sh
+++ bin/livebackground.sh
@@ -6,5 +6,8 @@
 
 
 # resolution=$(xrandr -q | grep -E "primary" | awk '{print $4}' | cut -f1 -d '+')
-resolution=$(xrandr -q | grep -E "primary" | awk '{print $4}')
+# resolution=$(xrandr -q | grep -E "primary" | awk '{print $4}')
+# resolution="2304x1296+3610+360"
+
+resolution=$(xrandr --listactivemonitors | tail -n+2 | awk '{print $2, $3}' | sed 's|/[0-9]\+x|x|;s|/[0-9]\++|+|' | fzf | awk '{print $2}')
 xwinwrap -g $resolution -ov -s -ni -b -nf -- livestream.sh "$1" WID
