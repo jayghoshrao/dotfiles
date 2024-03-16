324c0bffb74d5b5d2dd484455bdf16a869e9f7f4
Jayghosh Rao <jayghoshter@gmail.com>
2024-03-16 15:11:11 +0100

    separate out rofi-pomodoro

---
+++ bin/rofi-pomodoro.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+STATUS=$(~/bin/pomodoro-client.py status)
+
+[[ -n "$STATUS" ]] && row=1 || row=0 ;
+
+ARG=$(echo -e "  start\n  toggle\n  pause\n  resume\n  stop" | rofi -theme-str 'window {width: 22%;} listview { layout: horizontal; columns: 1; lines: 5; }' -dmenu -i -p Pomodoro -l 5 -selected-row $row | awk '{print $2}')
+[ -n $ARG ] && ~/bin/pomodoro-client.py $ARG
