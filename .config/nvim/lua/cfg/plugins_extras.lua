e122a8bfd733ef2f347352bd3374db9ac9a85ebd
Jayghosh Rao <clx-jrao@cds.int>
2025-11-17 21:41:53 +0100

    add codecompanion

--- .config/nvim/lua/cfg/plugins_extras.lua
+++ .config/nvim/lua/cfg/plugins_extras.lua
@@ -193,5 +193,20 @@ return {
     --         -- 'MeanderingProgrammer/render-markdown.nvim',
     --     },
     -- }
+
+    {
+        "olimorris/codecompanion.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        opts = {
+            -- NOTE: The log_level is in `opts.opts`
+            opts = {
+                log_level = "DEBUG", -- or "TRACE"
+            },
+        },
+    },
+
 }
 
