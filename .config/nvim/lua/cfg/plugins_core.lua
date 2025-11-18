b22e45c9633f669118612fc0bd1f177f15d28d3b
Jayghosh Rao <clx-jrao@cds.int>
2025-11-18 14:57:40 +0100

    fullscreen lazygit

--- .config/nvim/lua/cfg/plugins_core.lua
+++ .config/nvim/lua/cfg/plugins_core.lua
@@ -103,6 +103,7 @@ return {
         'kdheepak/lazygit.nvim',
         config = function()
             map('n', '<leader>gg', [[<cmd>LazyGit<cr>]])
+            vim.g.lazygit_floating_window_scaling_factor = 1.0 -- scaling factor for floating window
         end
     },
     {
