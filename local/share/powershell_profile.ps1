9b8bcd128d630cce84f1cc2895d905f539d25051
Jayghosh Rao <clx-jrao@cds.int>
2025-12-03 09:39:01 +0100

    update powershell

--- local/share/powershell_profile.ps1
+++ local/share/powershell_profile.ps1
@@ -12,6 +12,21 @@ function f {
 
 Set-Alias -Name lg -Value 'lazygit'
 
+# Dotfile management
+$env:DOTDIR = "$env:USERPROFILE\source\repos\dotfiles"
+
+function dot  { git -C $env:DOTDIR @args }
+function dots { git -C $env:DOTDIR status @args }
+function dotc { git -C $env:DOTDIR commit --verbose @args }
+function dota { git -C $env:DOTDIR add @args }
+function dotu { git -C $env:DOTDIR add --update @args }
+function dotd { git -C $env:DOTDIR diff @args }
+function dotr { git -C $env:DOTDIR rm @args }
+function dotlg { lazygit -p $env:DOTDIR }
+function dotlog { git -C $env:DOTDIR log --oneline --graph @args }
+
+Set-Alias -Name lgd -Value 'dotlg'
+
 $IsElevated = ([Security.Principal.WindowsPrincipal] `
     [Security.Principal.WindowsIdentity]::GetCurrent()
 ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
